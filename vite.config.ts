2d3710e6c4c64756e7ebc059bbdad40ae7a77e87
Sam Mortenson <samuel@mortenson.coffee>
2025-07-16 22:38:49 -0700

    Subdir

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/lexical-sync-demo",
   plugins: [react()],
   build: {
     rollupOptions: {
