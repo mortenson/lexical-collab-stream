a195e587253e8485dae45882304c186c8987b611
Sam Mortenson <mortensonsam@gmail.com>
2025-07-25 07:33:17 -0700

    Rename repo

--- examples/client/vite.config.ts
+++ examples/client/vite.config.ts
@@ -10,7 +10,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/lexical-sync-demo",
+  base: "/lexical-collab-stream",
   plugins: [react()],
   build: {
     rollupOptions: {
